3808546312ec1b0c77db31835a486b718a541d6c
Siwachanat Saengsuwan <161199258+Siwachanat@users.noreply.github.com>
2025-01-12 05:06:05 +0000

    Pending changes exported from your codespace

--- camp1.py
+++ camp1.py
@@ -1,1 +1,26 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
 
+start = 'A'
+goal = 'D'
+frontier = [start]
+exploror = set()
+
+print(frontier, exploror)
+
+while len(frontier) > 0:
+    current = frontier.pop()   
+    print(current)
+    
+    if current == goal:
+        print("Goalllllllll")
+        break
+    print(f"neighbor of {current} is {graph[current]}")
+    for neighbor in graph[current]:
+        frontier.append(neighbor)
